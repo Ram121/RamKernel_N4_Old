ca3d040c097b7e28f58a1b7223820846d2df2964
HRTKernel <support@hacker-kernel.com>
2016-01-23 23:47:29 +0100

    Linux 3.10.95 credits @UpInTheAir
    Linux 3.10.95: fix compile errors
    
    net/bluetooth/sco.c:466:6: error: 'addr_len' undeclared (first use in this function)
      if (addr_len < sizeof(struct sockaddr_sco))
    
    net/unix/af_unix.c:1920:6: error: unused variable 'noblock' [-Werror=unused-variable]
      int noblock = flags & MSG_DONTWAIT;
    
    Signed-off-by: UpInTheAir <upintheair.xda@gmail.com>

--- security/keys/gc.c
+++ security/keys/gc.c
@@ -189,8 +189,13 @@ static noinline void key_gc_unused_keys(struct list_head *keys)
 
 		/* Throw away the key data if the key is instantiated */
 		if (test_bit(KEY_FLAG_INSTANTIATED, &key->flags) &&
+<<<<<<< HEAD
 				!test_bit(KEY_FLAG_NEGATIVE, &key->flags) &&
 				key->type->destroy)
+=======
+		    !test_bit(KEY_FLAG_NEGATIVE, &key->flags) &&
+		    key->type->destroy)
+>>>>>>> d43a711... Linux 3.10.95 credits @UpInTheAir
 			key->type->destroy(key);
 
 		security_key_free(key);
