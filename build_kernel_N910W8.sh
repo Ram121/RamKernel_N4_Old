c2f1a52b9f3c696fb4bb6b8ceb3b91f9a7afb8d1
Ram <androlove11@gmail.com>
2016-11-03 00:14:22 +0530

    Changes for comman source of all models

---
+++ build_kernel_N910W8.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mv Makefile_N910W8 Makefile
+export ARCH=arm
+export CROSS_COMPILE=/opt/toolchains/arm-eabi-4.7/bin/arm-eabi-
+mkdir output
+
+make -C $(pwd) O=output VARIANT_DEFCONFIG=apq8084_sec_trlte_can_defconfig apq8084_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -C $(pwd) O=output
+
+cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
