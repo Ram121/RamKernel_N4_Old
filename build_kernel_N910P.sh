dc725c7ae8f15ff672489be575eec6ba377d4bae
Ram <androlove11@gmail.com>
2016-11-02 12:23:24 +0530

    Changes for comman source of all models

---
+++ build_kernel_N910P.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export ARCH=arm
+export CROSS_COMPILE=/opt/toolchains/UBERTC-arm-eabi-4.8/bin/arm-eabi-
+mkdir output
+
+make -C $(pwd) O=output apq8084_sec_defconfig VARIANT_DEFCONFIG=apq8084_sec_trlte_spr_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -C $(pwd) O=output
+
+cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
