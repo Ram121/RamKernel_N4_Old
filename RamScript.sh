8ff8586013f078303c23052dc0d63f3df7d6039c
Ram <androlove11@gmail.com>
2016-11-14 19:30:42 +0530

    Script updated for cleanup

--- RamScript.sh
+++ RamScript.sh
@@ -36,6 +36,12 @@ wait
 
 cp $(pwd)/bootimg/AIK-Linux-2.4/image-new.img $(pwd)/bootimg/zips/template/boot.img
 
+for i in $(pwd)/bootimg/AIK-Linux-2.4/cleanup.sh
+do
+"$i" &
+done
+wait
+
 7z a -tzip -mx5 $(pwd)/bootimg/zips/RamKernel_N7Port_N910G.zip $(pwd)/bootimg/zips/template/META-INF $(pwd)/bootimg/zips/template/root $(pwd)/bootimg/zips/template/system $(pwd)/bootimg/zips/template/boot.img $(pwd)/bootimg/zips/template/data
 
 rm $(pwd)/bootimg/zips/template/boot.img
