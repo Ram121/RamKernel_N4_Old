ebefd1e46bde44dc588a5a251742e4f2ad973811
Ram <androlove11@gmail.com>
2016-11-25 20:31:25 +0530

    More corrections

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -1186,8 +1186,6 @@ out:
 	return ret;
 }
 
-<<<<<<< HEAD
-=======
 static int pagemap_open(struct inode *inode, struct file *file)
 {
 
@@ -1199,8 +1197,6 @@ static int pagemap_open(struct inode *inode, struct file *file)
 		return -EPERM;
 	return 0;
 }
-
->>>>>>> dd8a0e8... Linux 3.10.61 to Linux 3.10.96
 const struct file_operations proc_pagemap_operations = {
 	.llseek		= mem_lseek, /* borrow this */
 	.read		= pagemap_read,
