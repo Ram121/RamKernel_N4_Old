65adfc22b43f61205f430b0d5985d00862c533c5
Ram <androlove11@gmail.com>
2016-11-28 18:33:34 +0530

    Script to compile for all models

---
+++ build_all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+./build_kernel.sh 910 G Y
+
+./build_kernel.sh 910 V Y
+
+./build_kernel.sh 910 P Y
+
+./build_kernel.sh 910 T Y
+
+./build_kernel.sh 910 W8 Y
+
+./build_kernel.sh 915 F Y
+
+echo "Zip made for all models"
